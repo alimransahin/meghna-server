b521685a622a701469ccfb439ac18dc9396e3e2d
alimransahin <92633905+alimransahin@users.noreply.github.com>
2022-11-09 11:38:45 +0600

    Data load successfull from mongo db

--- index.js
+++ index.js
@@ -11,11 +11,24 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.vgokw2y.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run(){
+    try{
+        const servicesCollection = client.db('meghna-tourist').collection('services');
+        app.get('/services', async (req,res)=>{
+            const query={};
+            const cursor=servicesCollection.find(query);
+            const services=await cursor.toArray();
+            res.send(services);
+        })
+
+
+    }
+    finally{
+
+    }
+}
+run().catch(err=>console.error(err));
 
 
 
