4028d74b1f93564c862fc2662fc26c95d89f2eea
alimransahin <92633905+alimransahin@users.noreply.github.com>
2022-11-09 18:27:44 +0600

    document find one complete

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const cors=require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app= express();
-const port= process.env.PORT||5000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
@@ -12,23 +12,35 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.vgokw2y.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         const servicesCollection = client.db('meghna-tourist').collection('services');
-        app.get('/services', async (req,res)=>{
-            const query={};
-            const cursor=servicesCollection.find(query);
-            const services=await cursor.toArray();
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
             res.send(services);
+        });
+        app.get('/servicesHome', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
         })
 
 
     }
-    finally{
+    finally {
 
     }
 }
-run().catch(err=>console.error(err));
+run().catch(err => console.error(err));
 
 
 
